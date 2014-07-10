b21155513f8912eda3ca53ee5a11dd241658b4da
Luke Bartel <bartelluke@gmail.com>
2014-07-10 13:10:43 -0600

    working on code

--- app.rb
+++ app.rb
@@ -1,14 +1,74 @@
 require "sinatra"
 require "active_record"
 require "./lib/database_connection"
+require "rack-flash"
 
 class App < Sinatra::Application
+  enable :sessions
+  use Rack::Flash
+
   def initialize
     super
     @database_connection = DatabaseConnection.new(ENV["RACK_ENV"])
   end
 
   get "/" do
-    erb :root
+    if session[:user_id]
+      current_user = @database_connection.sql("Select * FROM users where id = #{session[:user_id]}").first
+      users = @database_connection.sql("SELECT username from users where id != #{session[:user_id]}")
+      erb :logged_in, locals: {:users => users, :current_user => current_user}
+    else
+      erb :root
+    end
   end
+
+  get "/register" do
+    erb :register
+  end
+
+  post "/register" do
+    if params[:username] == ""
+      flash.now[:notice] = "Username cannot be blank"
+      erb :register
+    elsif params[:password] == ""
+      flash.now[:notice] = "Password cannot be blank"
+      erb :register
+    elsif register_user_exists?(params[:username])
+      flash.now[:notice] = "Username already exists"
+      erb :register
+    else
+      @database_connection.sql("INSERT INTO users (username, password) values ('#{params[:username]}','#{params[:password]}')")
+      flash[:notice] = "Thank you for registering"
+      redirect '/'
+    end
+  end
+
+  post "/login" do
+    id_array = @database_connection.sql("SELECT id FROM users WHERE username ='#{params[:username]}' and password = '#{params[:password]}'")
+    id_hash = id_array.first
+    id = id_hash["id"]
+    session[:user_id] = id
+    redirect '/'
+  end
+
+  get "/sort" do
+    users = @database_connection.sql("SELECT username from users where id != #{session[:user_id]} by username")
+    erb :sort, locals: {:users => users}
+  end
+
+
+
+  get "/logout" do
+    session.clear
+    redirect '/'
+  end
+
+  private
+
+  def register_user_exists?(username)
+    id_array = @database_connection.sql("SELECT id FROM users WHERE username ='#{username}'")
+    if id_array
+    end
+  end
+
 end
--- spec/features/sinatra_crud_spec.rb
+++ spec/features/sinatra_crud_spec.rb
@@ -3,6 +3,66 @@ require "spec_helper"
 feature "User Authentication" do
   scenario "user can register and login" do
     visit "/"
-    click_on "Registration"
+    click_on "Register"
+    visit "/register"
+    expect(page).to have_content "Username"
+    expect(page).to have_content "Password"
+
+    fill_in "Username", :with => "todd"
+    fill_in "Password", :with => "todd"
+
+    click_on "Sign Up"
+
+    expect(page).to have_content "Thank you for registering"
+    expect(page).to have_content "Username"
+    expect(page).to have_content "Password"
+
+
+    fill_in "Username", :with => "todd"
+    fill_in "Password", :with => "todd"
+
+    click_on "Login"
+
+    click_on "Logout"
+
   end
+  scenario "check validations" do
+    visit "/"
+    click_on "Register"
+
+    fill_in "Username", :with => ""
+
+    click_on "Sign Up"
+
+    expect(page).to have_content "Username cannot be blank"
+  end
+
+  scenario "As a logged in user" do
+    visit "/"
+    click_on "Register"
+    visit "/register"
+    expect(page).to have_content "Username"
+    expect(page).to have_content "Password"
+
+    fill_in "Username", :with => "todd"
+    fill_in "Password", :with => "todd"
+
+    click_on "Sign Up"
+
+    expect(page).to have_content "Thank you for registering"
+    expect(page).to have_content "Username"
+    expect(page).to have_content "Password"
+
+
+    fill_in "Username", :with => "todd"
+    fill_in "Password", :with => "todd"
+
+    click_on "Login"
+
+    expect(page).to have_content "Users"
+    expect(page).to have_content "bob"
+
+    click_on "Sort"
+
+end
 end
