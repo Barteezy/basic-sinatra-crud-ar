3843d633f4740e10fba913513a4be18cbed39fc8
Luke Bartel <bartelluke@gmail.com>
2014-07-15 19:22:05 -0600

    just started css

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 require "sinatra"
 require "active_record"
-
+require "rack-flash"
 require "gschool_database_connection"
 
 class App < Sinatra::Application
@@ -9,7 +9,7 @@ class App < Sinatra::Application
 
   def initialize
     super
-    @database_connection = GschoolDatabseConnection::DatabaseConnection.establish(ENV["RACK_ENV"])
+    @database_connection = GschoolDatabaseConnection::DatabaseConnection.establish(ENV["RACK_ENV"])
   end
 
   get "/" do
